f7cd7f17d944a392c13058189a651984c6597296
SOUVIK SAHOO <sahoo2003souvik@gmail.com>
2025-11-09 23:17:17 +0530

    fix: added use client directive to profile page

--- app/(dashboard)/(routes)/teams/page.tsx
+++ app/(dashboard)/(routes)/teams/page.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import PostsTab from './components/PostsTab';
-import {FilterTab} from './components/FilterTab';
+"use client";
 
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
-const page = () => {
+export default function ProfilePage() {
+  const { data: session } = useSession();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (session) {
+      setUser(session.user);
+    }
+  }, [session]);
+
   return (
-    <div className='min-h-screen text-black dark:bg-background bg-background dark:text-white  '>
-      
-      <div className='grid grid-cols-1 lg:grid-cols-4 gap-2 w-full mt-10 pr-5  '>
-        <FilterTab />
-        <div className='col-span-3 px-5 lg:px-0 pt-4'>
-        <PostsTab />
-        </div>
-      </div>      
+    <div>
+      <h1>Welcome, {user?.name}</h1>
+      <Link href="/dashboard">Back to Dashboard</Link>
     </div>
-  )
+  );
 }
-
-export default page
